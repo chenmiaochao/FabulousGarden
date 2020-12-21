bdbb74fc7930fd394287e9a20de209431c8f6dce
chenrongchao <chenmiaochaonp@gmail.com>
2020-12-21 19:12:36 +0900

    first form with validate

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,37 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <global-header :user="currentUser"></global-header>
     <post-list :list="list"></post-list>
-    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <validate-form @form-submit="onFormSubmit">
+        <div class="mb-3">
+        <label class="form-label">メールアドレス</label>
+        <validate-input
+          :rules="emailRules" v-model="emailVal"
+          placeholder="メルアドレスを入力してください"
+          type="text"
+          ref="inputRef"
+        />
+        </div>
+        <div class="mb-3">
+        <label class="form-label">コミュニティ名</label>
+        <validate-input
+          :rules="communityNameRules" v-model="communityNameVal"
+          placeholder="コミュニティ名を入力してください"
+          type="text"
+          ref="inputRef"
+        />
+        </div>
+    </validate-form>
   </div>
 </template>
 
 <script lang="ts">
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, reactive, ref } from 'vue'
 // import HelloWorld from './components/HelloWorld.vue'
 import PostList, { PostProps } from './components/PostList.vue'
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+import ValidateInput, { RulesProp } from './components/ValidateInput.vue'
+import ValidateForm from './components/ValidateForm.vue'
 const currentUser: UserProps = {
   isLogin: true,
   name: 'えい',
@@ -51,13 +72,40 @@ export default defineComponent({
   name: 'App',
   components: {
     PostList,
-    GlobalHeader
+    GlobalHeader,
+    ValidateInput,
+    ValidateForm
   },
   setup () {
+    const inputRef = ref<any>()
+    const emailVal = ref('123@test.com')
+    const emailRules: RulesProp = [
+      { type: 'required', message: '电子邮箱不能为空' },
+      { type: 'email', message: '请输入正确的电子邮箱格式' }
+    ]
+    const passwordVal = ref('123')
+    const passwordRules: RulesProp = [
+      { type: 'required', message: '请输入密码' }
+    ]
+    const communityNameVal = ref('ビッチzoon')
+    const communityNameRules: RulesProp = [
+      { type: 'required', message: 'コミュニティ名を入力してください' }
+    ]
+    const onFormSubmit = (result: boolean) => {
+      console.log('result', result)
+    }
     console.log(currentUser)
     return {
       list: testData,
-      currentUser
+      currentUser,
+      inputRef,
+      emailVal,
+      emailRules,
+      passwordVal,
+      passwordRules,
+      communityNameRules,
+      communityNameVal,
+      onFormSubmit
     }
   }
 })
---
+++ src/components/ValidateInput.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="validation-input-container pb-3">
+    <input
+      class="form-control"
+      :class="{'is-invalid': inputRef.error}"
+      :value="inputRef.val"
+      @blur="validateInput"
+      @input="updateValue"
+      v-bind="$attrs"
+    >
+    <span v-if="inputRef.error" class="invalid-feedback">{{inputRef.message}}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, PropType, onMounted } from 'vue'
+import { emitter } from './ValidateForm.vue'
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+interface RuleProp {
+  type: 'required' | 'email';
+  message: string;
+}
+export type RulesProp = RuleProp[]
+export default defineComponent({
+  // 子组件实现父组件v-model绑定 两步走
+  // 第一步modelValue in props
+  props: {
+    rules: Array as PropType<RulesProp>,
+    modelValue: String
+  },
+  inheritAttrs: false,
+  setup (props, context) {
+    // console.log(context.attrs)
+    const inputRef = reactive({
+      val: props.modelValue || '',
+      error: false,
+      message: ''
+    })
+    const updateValue = (e: KeyboardEvent) => {
+      // 输入的值
+      const targetValue = (e.target as HTMLInputElement).value
+      // 输入的值带入ref
+      inputRef.val = targetValue
+      // 第二步emit去父组件
+      context.emit('update:modelValue', targetValue)
+    }
+    const validateInput = () => {
+      if (props.rules) {
+        const allPassed = props.rules.every(rule => {
+          let passed = true
+          inputRef.message = rule.message
+          switch (rule.type) {
+            case 'required':
+              passed = (inputRef.val.trim() !== '')
+              break
+            case 'email':
+              passed = emailReg.test(inputRef.val)
+              break
+            default:
+              break
+          }
+          return passed
+        })
+        inputRef.error = !allPassed
+        return allPassed
+      }
+      return true
+    }
+    onMounted(() => {
+      emitter.emit('form-item-created', validateInput)
+    })
+    return {
+      inputRef,
+      validateInput,
+      updateValue
+    }
+  }
+})
+</script>
