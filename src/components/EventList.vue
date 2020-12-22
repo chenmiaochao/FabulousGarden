ca8940a220d0181884c0a636496a45198fe05e36
chenrongchao <chenmiaochaonp@gmail.com>
2020-12-22 23:31:47 +0900

    event list pre-fin

---
+++ src/components/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="box overflow-scroll flex-nowrap">
+    <div v-for="event in list2" :key="event.id" class="mb-4  box-item rounded-circle">
+      <div class="">
+        <div class="card-body text-center">
+          <img  :src="event.avatar" :alt="event.title" class=" border border-light w-25 my-3" >
+          <h5 class="card-title">{{event.title}}</h5>
+          <a href="#" class="btn btn-outline-primary">img</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export interface EventProps{
+  id: number;
+  title: string;
+  avatar?: string;
+}
+export default defineComponent({
+  name: 'EventList',
+  props: {
+    list2: {
+      type: Array as PropType<EventProps[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style>
+.box{
+  display: flex;
+}
+.box-item{
+  height: 80px;
+  width: 80px;
+  background-color: aqua;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home-page">
+
     <section class="py-5 text-center container">
+      <event-list :list2="list2"></event-list>
       <div class="row py-lg-5">
         <div class="col-lg-6 com-md-8 mx-auto">
           <img src="../assets/4.png" alt="callout" class="w-50"/>
@@ -19,32 +21,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import PostList, { PostProps } from '../components/PostList.vue'
-// const testData: ColumnProps[] = [
-//   {
-//     id: 1,
-//     title: 'test1的专栏',
-//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 2,
-//     title: 'test2的专栏',
-//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧'
-//     // avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 3,
-//     title: 'test3的专栏',
-//     description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   },
-//   {
-//     id: 4,
-//     title: 'test4的专栏',
-//     description: '这是的test2专栏，有一段非常有意思的简介，可以更新一下欧',
-//     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
-//   }
-// ]
+import EventList, { EventProps } from '../components/EventList.vue'
+
 const testData: PostProps[] = [
   {
     id: 1,
@@ -71,14 +49,58 @@ const testData: PostProps[] = [
     avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
+const testData2: EventProps[] = [
+  {
+    id: 1,
+    title: 'Post1',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 2,
+    title: 'post2',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 3,
+    title: 'test3',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 4,
+    title: 'post4',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 5,
+    title: 'Post1',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 6,
+    title: 'post2',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 7,
+    title: 'test3',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  },
+  {
+    id: 8,
+    title: 'post4',
+    avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+  }
+]
 export default defineComponent({
   name: 'Home',
   components: {
-    PostList
+    PostList,
+    EventList
   },
   setup () {
     return {
-      list: testData
+      list: testData,
+      list2: testData2
     }
   }
 })
